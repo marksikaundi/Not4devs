b134e6a54d928149e3de451f5ebce9e4b721a5ea
marksikaundi <emmanuelmark117@gmail.com>
2023-04-30 23:19:45 +0200

    fixed

--- App.js
+++ App.js
@@ -1,36 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 
 import { StyleSheet, Text, View, Image } from "react-native";
-import {
-  Entypo,
-  AntDesign,
-  FontAwesome5,
-  MaterialCommunityIcons,
-} from "@expo/vector-icons";
-import LikeImage from "./assets/images/like.png";
 import FeedPost from "./src/components/FeedPost";
-
-
-const post = {
-  id: "p1",
-  createdAt: "20 min ago",
-  User: {
-    id: "u1",
-    image:
-      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/zuck.jpeg",
-    name: "Mark Sikaundi",
-  },
-  description:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-  image: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg",
-  numberOfLikes: 11,
-  numberOfShares: 2,
-};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      
+      <FeedPost />
       <FeedPost />
       <StatusBar style="auto" />
     </View>
@@ -39,78 +15,10 @@ export default function App() {
 
 //Styles
 const styles = StyleSheet.create({
-  post: {
+  container: {
+    flex: 1,
     backgroundColor: "#fff",
-    marginVertical: 5,
-  },
-  header: {
-    padding: 10,
-    flexDirection: "row",
     alignItems: "center",
-    width: "100%",
-  },
-  profileImage: {
-    width: 40,
-    height: 40,
-    borderRadius: 25,
-    marginRight: 10,
-  },
-  name: {
-    fontWeight: "500",
-  },
-  subtitle: {
-    color: "gray",
-  },
-  icon: {
-    marginLeft: "auto",
-  },
-
-  description: {
-    paddingHorizontal: 10,
-    lineHeight: 20,
-    letterSpacing: 0.4,
-  },
-  image: {
-    width: "100%",
-    aspectRatio: 1,
-    marginTop: 10,
-  },
-
-  //footer
-  footer: {
-    paddingHorizontal: 10,
-  },
-  statsRaw: {
-    paddingVertical: 10,
-    flexDirection: "row",
-    borderBottomWidth: StyleSheet.hairlineWidth,
-    borderColor: "lightgrey",
-  },
-  likeIcon: {
-    width: 20,
-    height: 20,
-    marginRight: 5,
-  },
-  likedBy: {
-    color: "grey",
-  },
-  shares: {
-    marginLeft: "auto",
-    color: "grey",
-  },
-  // Buttons Row
-  buttonsRow: {
-    marginVertical: 10,
-    flexDirection: "row",
-    justifyContent: "space-around",
-  },
-  iconButton: {
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  iconButtonText: {
-    color: "gray",
-    marginLeft: 5,
-    fontWeight: "500",
+    justifyContent: "center",
   },
 });
