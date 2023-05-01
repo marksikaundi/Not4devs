b2ecf1d11b280ecdeea873db318bbe2bd01caded
marksikaundi <emmanuelmark117@gmail.com>
2023-05-01 17:30:10 +0200

    UPDATES

--- src/screens/CreatePostScreen.js
+++ src/screens/CreatePostScreen.js
@@ -9,17 +9,38 @@ const user = {
 
 const CreatePostScreen = () => {
   return (
-    <View style={StyleSheet.container}>
-      <View>
-        <Image source={{ uri: user.image }} />
-        <Text>{user.name}</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={{ uri: user.image }} style={styles.image} />
+        <Text style={styles.name}>{user.name}</Text>
       </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    width: "100%",
+    padding: 20,
+    marginTop: 20,
+    backgroundColor: "#fff",
+    paddingTop: 30,
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+  },
+  image: {
+    height: 40,
+    window: 40,
+    borderRadius: 30,
+    marginRight: 10,
+  },
+  name: {
+    fontWeight: "500",
+  },
 });
 
 export default CreatePostScreen;
