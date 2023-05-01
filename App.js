0464420581f5cae0a3852f0a7e4da989bb5758cd
marksikaundi <emmanuelmark117@gmail.com>
2023-05-01 17:11:27 +0200

    implemeting create post component

--- App.js
+++ App.js
@@ -2,12 +2,12 @@ import { StatusBar } from "expo-status-bar";
 
 import { StyleSheet, FlatList, Text, View, Image } from "react-native";
 import FeedScreen from "./src/screens/FeedScreen";
+import CreatePostScreen from "./src/screens/CreatePostScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      
-      <FeedScreen />
+      <CreatePostScreen />
     </View>
   );
 }
---
+++ src/screens/CreatePostScreen.js
@@ -0,0 +1,18 @@
+import { Text, StyleSheet, View } from "react-native";
+
+const CreatePostScreen = () => {
+  return (
+    <View style={StyleSheet.container}>
+      <View>
+        <Image />
+        <Text></Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {},
+});
+
+export default CreatePostScreen;
